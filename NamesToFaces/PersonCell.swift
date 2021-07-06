cf76a12bc7445d7b017347884f045b5c475ef7fe
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-07-06 08:28:53 +0300

    Completed Day 1

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Igor Chernyshov on 06.07.2021.
+//
+
+import UIKit
+
+final class PersonCell: UICollectionViewCell {
+
+	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var nameLabel: UILabel!
+}
